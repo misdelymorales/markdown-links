cbb7fa099112a2e2022400999cdaec2f38781afa
Misdely Morales <misdely.m@gmail.com>
2022-10-18 18:34:55 -0300

    muestra rurta absoluta

---
+++ cli.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+const { mdLinks } = require("./index.js");
+
+console.log("-------------------------------------------");
+console.log("         Bienvenido a memb-mdlinks         ");
+console.log("-------------------------------------------");
+
+const saveRoute = process.argv[2];
